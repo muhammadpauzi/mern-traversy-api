687bf73dcffad25d0d293b1cb7ee3e758c175ccb
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-21 09:54:14 +0700

    create user model and update goal model

--- models/goal.model.js
+++ models/goal.model.js
@@ -4,7 +4,12 @@ const goalSchema = mongoose.Schema(
     {
         text: {
             type: String,
-            required: [true, 'Please add text value'],
+            required: [true, 'Please add a text value'],
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
         },
     },
     { timestamps: true }
